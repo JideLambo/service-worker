88009443108c872792f41c47119b76d57cb81622
jidelambo <jidelambo@gmail.com>
2018-05-10 13:48:49 +0100

    Initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,51 +1,14 @@
+//find out if service-worker is available in browser
+//if available, initiate service worker
+
 if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker
-    .register('./service-worker.js', { scope: './' })
-    .then(function(registration) {
-      console.log("Service Worker Registered");
+    .register('./service-worker.js', {scope: './'})
+    .then(function(reg) {
+      console.log("service worker registered", reg)
     })
-    .catch(function(err) {
-      console.log("Service Worker Failed to Register", err);
+    .catch(function(err){
+      console.log("service worker failed to register", err)
     })
-
 }
-
-
-
-// Function to perform HTTP request
-var get = function(url) {
-  return new Promise(function(resolve, reject) {
-
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var result = xhr.responseText
-                result = JSON.parse(result);
-                resolve(result);
-            } else {
-                reject(xhr);
-            }
-        }
-    };
-    
-    xhr.open("GET", url, true);
-    xhr.send();
-
-  }); 
-};
-
-
-get('https://api.nasa.gov/planetary/earth/imagery?api_key=fWfSMcDzyHfMuH3BW6jiIUBYaj3hKRyKBRTBqgEQ')
-  .then(function(response) {
-    // There is an issue with the image being pulled from the API, so using a different one instead
-    document.getElementsByClassName('targetImage')[0].src = "https://api.nasa.gov/images/earth.png";
-
-  })
-  .catch(function(err) {
-    console.log("Error", err);
-  })
-
-
-
